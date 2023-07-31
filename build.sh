02f2dc51e311b7da46f936101f2c78f54d5b5912
muncherelli <16467105+muncherelli@users.noreply.github.com>
2023-07-31 16:55:50 -0500

    rebuilt file

--- build.sh
+++ build.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-# Parse command line arguments
-while [[ $# -gt 0 ]]
-do
-key="$1"
+# Function to display usage information
+usage() {
+    echo "Usage: $0 -o|--ols OPENLITESPEED_VERSION -p|--php PHP_VERSION"
+    exit 1
+}
 
-case $key in
-    --ols)
-    OPENLITESPEED_VERSION="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    --php)
-    PHP_VERSION="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    shift # past argument
-    ;;
-esac
+# Parse command line arguments
+while (( "$#" )); do
+  case "$1" in
+    -o|--ols)
+      OPENLITESPEED_VERSION=$2
+      shift 2
+      ;;
+    -p|--php)
+      PHP_VERSION=$2
+      shift 2
+      ;;
+    *)
+      usage
+      ;;
+  esac
 done
 
 # Ensure OPENLITESPEED_VERSION and PHP_VERSION arguments are passed
 if [ -z "$OPENLITESPEED_VERSION" ] || [ -z "$PHP_VERSION" ]; then
-    echo "Two arguments required: --ols OPENLITESPEED_VERSION and --php PHP_VERSION"
-    exit 1
+    echo "Two arguments required: -o|--ols OPENLITESPEED_VERSION and -p|--php PHP_VERSION"
+    usage
 fi
 
 # Docker image tag
@@ -33,6 +34,9 @@ TAG="${OPENLITESPEED_VERSION}-php${PHP_VERSION}"
 
 # Build the Docker image
 echo "Building Docker image..."
-docker build -t muncherelli/openlitespeed:$TAG --build-arg OPENLITESPEED_VERSION=$OPENLITESPEED_VERSION --build-arg PHP_VERSION=$PHP_VERSION .
-
-echo "Done."
+if docker build -t muncherelli/openlitespeed:$TAG --build-arg OPENLITESPEED_VERSION=$OPENLITESPEED_VERSION --build-arg PHP_VERSION=$PHP_VERSION . ; then
+    echo "Docker image built successfully."
+else
+    echo "Failed to build Docker image."
+    exit 1
+fi
