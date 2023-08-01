adbd0bd384dabe8697a9bd56c4ea020b44a04db9
muncherelli <16467105+muncherelli@users.noreply.github.com>
2023-07-31 19:58:40 -0500

    added args

--- push.sh
+++ push.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# Parse command line arguments
-while [[ $# -gt 0 ]]
-do
-key="$1"
+# Function to display usage information
+usage() {
+    echo "Usage: $0 -o|--ols OPENLITESPEED_VERSION -p|--php PHP_VERSION"
+    exit 1
+}
 
-case $key in
-    --ols)
-    OPENLITESPEED_VERSION="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    --php)
-    PHP_VERSION="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    *)    # unknown option
-    shift # past argument
-    ;;
-esac
+# Parse command line arguments
+while (( "$#" )); do
+  case "$1" in
+    -o|--ols)
+      OPENLITESPEED_VERSION=$2
+      shift 2
+      ;;
+    -p|--php)
+      PHP_VERSION=$2
+      shift 2
+      ;;
+    *)
+      usage
+      ;;
+  esac
 done
 
 # Ensure OPENLITESPEED_VERSION and PHP_VERSION arguments are passed
 if [ -z "$OPENLITESPEED_VERSION" ] || [ -z "$PHP_VERSION" ]; then
-    echo "Two arguments required: --ols OPENLITESPEED_VERSION and --php PHP_VERSION"
-    exit 1
+    usage
 fi
 
 # Docker Hub username
